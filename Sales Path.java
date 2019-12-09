85ab29d17b6fb2ad3fe078015ca7658ad1f7041d
kumag <kumar420gaurav@gmail.com>
2019-12-09 22:41:12 +0530

    Update Sales Path.java

--- Sales Path.java
+++ Sales Path.java
@@ -1,5 +1,6 @@
 //Word Count Engine
 //https://www.pramp.com/challenge/15oxrQx6LjtQj9JK9XqA
+// BFS iterative
 import java.io.*;
 import java.util.*;
 
@@ -90,3 +91,89 @@ class Solution {
     
   }
 }
+
+//Recursive
+import java.io.*;
+import java.util.*;
+
+class Solution {
+ 
+  static class Node {
+      
+    int cost;
+    Node[] children;
+    Node parent;
+
+    Node(int cost) {
+      this.cost = cost;
+      children = null;
+      parent = null;
+    }
+  }
+
+  static class SalesPath {
+        
+    int getCheapestCost(Node node) {
+      if(node == null){
+        return 0;
+      }
+      if(node.children == null){
+        return node.cost;
+      }
+      int min = Integer.MAX_VALUE;
+      for(Node child: node.children){
+        int cost = getCheapestCost(child);
+        if(cost < min){
+          min = cost;
+        }
+      }
+      return min + node.cost;
+    }
+  }
+  public static boolean isLeaf(Node node){
+    if(node == null){
+      return false;
+    }
+    if(node.children == null){
+      return true;
+    }
+    return false;
+  }
+    
+  /*********************************************
+   * Driver program to test above method     *
+   *********************************************/
+
+  public static void main(String[] args) {
+    Node root = new Node(0);
+    root.children = new Node[3];
+    root.children[0] = new Node(5);
+    root.children[1] = new Node(3);
+    root.children[2] = new Node(6);
+    
+    root.children[0].children = new Node[1];
+    root.children[0].children[0] = new Node(4);
+    
+    root.children[1].children = new Node[2];
+    root.children[1].children[0] = new Node(2);
+    root.children[1].children[1] = new Node(0);
+    
+    root.children[1].children[0].children = new Node[1];
+    root.children[1].children[0].children[0] = new Node(1);
+    root.children[1].children[0].children[0].children = new Node[1];
+    root.children[1].children[0].children[0].children[0] = new Node(1);
+    
+    root.children[1].children[1].children = new Node[1];
+    root.children[1].children[1].children[0] = new Node(10);
+    
+    root.children[2].children = new Node[2];
+    root.children[2].children[0] = new Node(1);
+    root.children[2].children[1] = new Node(5);
+    
+    
+    int ans = new SalesPath().getCheapestCost(root);
+    System.out.println(ans);
+    
+  }
+}
+
