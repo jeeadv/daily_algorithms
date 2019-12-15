db66104f2d7afd83214f9d51c35f87dde2f892ba
kumag <kumar420gaurav@gmail.com>
2019-12-16 01:45:27 +0530

    Update Find The Duplicates.java

--- Find The Duplicates.java
+++ Find The Duplicates.java
@@ -1,11 +1,46 @@
 //Find The Duplicates
 //https://www.pramp.com/challenge/15oxrQx6LjtQj9JK9XlA
+
 import java.io.*;
 import java.util.*;
 
 class Solution {
-
+  static boolean search(int[] arr, int k, int l, int r){
+    if(l <= r){
+      int mid = (l+r)/2;
+      if(arr[mid] == k){
+        return true;
+      }
+      else if(arr[mid]>k){
+        return search(arr, k, l, mid-1);
+      }
+      else{
+        return search(arr, k, mid+1, r);
+      }
+    }
+    else{
+      return false;
+    }
+  }
   static int[] findDuplicates(int[] arr1, int[] arr2) {
+    int m = arr1.length;
+    int n = arr2.length;
+    
+    List<Integer> list = new ArrayList<>();
+    for(int i=0;i<m;i++){
+      if(search(arr2, arr1[i], 0, n-1)){
+        list.add(arr1[i]);
+      }
+    }
+    int[] ans = new int[list.size()];
+    int k = 0;
+    for(int e: list){
+      ans[k++] = e;
+    }
+    return ans;
+  }
+  
+  static int[] findDuplicates1(int[] arr1, int[] arr2) {
     int m = arr1.length;
     int n = arr2.length;
     int i = 0;
