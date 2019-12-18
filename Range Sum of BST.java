ebdd1c3752ce113ef96aca9b2ab6ab6f96d67826
kumag <kumar420gaurav@gmail.com>
2019-12-19 01:44:28 +0530

    Update Range Sum of BST.java

--- Range Sum of BST.java
+++ Range Sum of BST.java
@@ -110,3 +110,32 @@ class Solution {
         return sum;
     }
 }
+// recursive 2
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if(root == null){
+            return 0;
+        }
+        int sum = 0;
+        if(root.val >= L && root.val <= R){
+            sum += root.val;
+        }
+        if(root.val > L){
+            sum += rangeSumBST(root.left, L, R);
+        }
+        if(root.val < R){
+            sum += rangeSumBST(root.right, L, R);
+        }
+        return sum;
+    }
+}
