2b428b754778d5c66aebd0e4f44493efea1a8dc9
kumag <kumar420gaurav@gmail.com>
2019-12-21 16:08:17 +0530

    Update N-ary Tree Postorder Traversal.java

--- N-ary Tree Postorder Traversal.java
+++ N-ary Tree Postorder Traversal.java
@@ -36,3 +36,44 @@ class Solution {
         list.add(root.val);
     }
 }
+
+//iterative
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> postorder(Node root) {
+        LinkedList<Integer> list = new LinkedList<>();
+        if(root == null){
+            return list;
+        }
+        
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            Node top = stack.pop();
+            for(Node child: top.children){
+                stack.push(child);
+            }
+            list.addFirst(top.val);
+        }
+        
+        return list;
+        
+    }
+}
