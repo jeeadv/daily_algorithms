79a4f570f8eb95b6c9c73bcf629b5b167f168393
kumag <kumar420gaurav@gmail.com>
2019-12-21 15:52:18 +0530

    Create N-ary Tree Postorder Traversal.java

---
+++ N-ary Tree Postorder Traversal.java
@@ -0,0 +1,38 @@
+//N-ary Tree Postorder Traversal
+//https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> list = new LinkedList<>();
+        postorder(root, list);
+        return list;
+        
+    }
+    public void postorder(Node root, List<Integer> list){
+        if(root == null){
+            return;
+        }
+        for(Node child: root.children){
+            postorder(child, list);
+        }
+        list.add(root.val);
+    }
+}
