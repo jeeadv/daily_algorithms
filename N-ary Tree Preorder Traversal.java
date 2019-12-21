54ab57401d97a232eb7ddf434f83069d623e995c
kumag <kumar420gaurav@gmail.com>
2019-12-21 16:28:40 +0530

    Update N-ary Tree Preorder Traversal.java

--- N-ary Tree Preorder Traversal.java
+++ N-ary Tree Preorder Traversal.java
@@ -35,3 +35,43 @@ class Solution {
         }
     }
 }
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new LinkedList<>();
+        if(root == null){
+            return list;
+        }
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            Node top = stack.pop();
+            list.add(top.val);
+            if(top.children != null){
+                ListIterator<Node> itr = top.children.listIterator(top.children.size());
+                while(itr.hasPrevious()){
+                    stack.push(itr.previous());
+                }
+            }
+        }
+        return list;
+    }
+}
