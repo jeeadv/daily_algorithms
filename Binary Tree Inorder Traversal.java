47e22e59fadc4059d373b0e24be6f9036b0e995f
kumag <kumar420gaurav@gmail.com>
2019-12-23 21:17:45 +0530

    Update Binary Tree Inorder Traversal.java

--- Binary Tree Inorder Traversal.java
+++ Binary Tree Inorder Traversal.java
@@ -25,3 +25,39 @@ class Solution {
         inorder(root.right, list);
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        
+        if(root == null){
+            return list;
+        }
+        
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        
+        while(!stack.isEmpty() || curr != null){
+            
+            while(curr != null ){
+                stack.push(curr);
+                curr = curr.left;
+            }
+            
+            TreeNode top = stack.pop();
+            list.add(top.val);
+            curr = top.right;
+        }
+        
+        return list;
+    }
+}
