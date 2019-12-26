53e90506be4b906f006f215216ca1f1a8d0bcae0
kumag <kumar420gaurav@gmail.com>
2019-12-27 01:19:16 +0530

    Update Shifted Array Search.java

--- Shifted Array Search.java
+++ Shifted Array Search.java
@@ -6,7 +6,36 @@ import java.util.*;
 
 class Solution {
 
-  static int shiftedArrSearch(int[] shiftArr, int num) {
+  static int shiftedArrSearch(int[] arr, int num) {
+    int l = 0;
+    int r = arr.length - 1;
+    
+    while(l <= r){
+      int mid = (l + r)/2;
+      if(arr[mid] == num){
+        return mid;
+      }
+      else if(arr[l] <= arr[mid]){
+        if(num >= arr[l] && num <= arr[mid]){
+          r = mid - 1;
+        }
+        else{
+          l = mid + 1;
+        }
+      }
+      else{
+        if(num >= arr[mid] && num <= arr[r]){
+          l = mid + 1;
+        }
+        else{
+          r = mid - 1;
+        }
+      }
+    }
+    return -1;
+  }
+  
+  static int shiftedArrSearch1(int[] shiftArr, int num) {
     if(shiftArr[0] < shiftArr[shiftArr.length - 1]){
       return binarySearch(shiftArr, num, 0, shiftArr.length - 1);
     }
@@ -60,11 +89,8 @@ class Solution {
 
 }
 /*
-// can you hear?
-//can you hear me?
-
 [9, 12, 1, 2, 4, 5] , num = 2
- 0   1   2  3  4  5
+ 0   1  2  3  4  5
  (0,5) => 17
  
 */
