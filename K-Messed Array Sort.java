f02ff34ad7f93ea16638c3eea2ec2effee87f8f1
kumag <kumar420gaurav@gmail.com>
2019-12-31 17:15:27 +0530

    Update K-Messed Array Sort.java

--- K-Messed Array Sort.java
+++ K-Messed Array Sort.java
@@ -5,8 +5,27 @@ import java.util.*;
 class Solution {
 
   static int[] sortKMessedArray(int[] arr, int k) {
+    PriorityQueue<Integer> heap = new PriorityQueue<>();
+    for(int i=0;i<=k;i++){
+      heap.add(arr[i]);
+    }
+    
+    int j = 0;
+    for(int i=k+1;i<arr.length;i++){
+      arr[j++] = heap.remove();
+      heap.add(arr[i]);
+    }
+    
+    while(!heap.isEmpty()){
+      arr[j++] = heap.remove();
+    }
+    return arr;
+  }
+  
+  static int[] sortKMessedArray1(int[] arr, int k) {
     return insertionSort(arr, k);
   }
+  
   static int[] insertionSort(int[] arr, int k){
     int n = arr.length;
     for(int i=1;i<n;i++){
@@ -16,6 +35,7 @@ class Solution {
     }
     return arr;
   }
+  
   static void shiftArray(int[] arr, int r){
     while(r>=1){
       if(arr[r] < arr[r-1]){
@@ -27,6 +47,7 @@ class Solution {
       r--;
     }
   }
+  
   static void swap(int[] arr, int i, int j){
     int temp = arr[i];
     arr[i] = arr[j];
@@ -40,7 +61,8 @@ class Solution {
     System.out.println(Arrays.toString(res));
   }
 
-}/*
+}
+/*
 k = 2
 [4 3 2 1]
 [2 3 4 1]
