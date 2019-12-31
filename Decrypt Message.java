f52d8fd0d79416787ee80d72f48eb701b78703d8
kumag <kumar420gaurav@gmail.com>
2019-12-31 18:29:30 +0530

    Create Decrypt Message.java

---
+++ Decrypt Message.java
@@ -0,0 +1,61 @@
+//Decrypt Message
+//https://www.pramp.com/challenge/8noLWxLP6JUZJ2bA2rnx
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+  static String decrypt(String word) {
+    if(word == null || word.length() == 0){
+      return word;
+    }
+    
+    int[] arr = new int[word.length()];
+    arr[0] = word.charAt(0) - 1;
+    
+    while(arr[0] < 'a' || arr[0] > 'z'){
+      arr[0] += 26;
+    }
+    
+    for(int i=1;i<word.length();i++){
+      //arr[i] = word.charAt(i) - arr[i-1] - 1;
+      arr[i] = word.charAt(i) - word.charAt(i - 1);
+      while(arr[i] < 'a' || arr[i] > 'z'){
+        arr[i] += 26;
+      }
+    }
+    
+    String res = "";
+    for(int i=0;i<arr.length;i++){
+      res += (char)arr[i];
+    }
+    return res;
+  }
+
+  public static void main(String[] args) {
+    String word = "dnotq";
+    String ans = decrypt(word);
+    System.out.println(ans);
+  }
+
+}
+/*
+input:  word = "dnotq"
+output: "crime"
+
+Decrypted message:	c	   r	 i	m	   e
+            Step 1:	99	114	105	109	101
+            Step 2:	100	214	319	428	529
+            Step 3:	100	110	111	116	113
+Encrypted message:	d	   n	 o	t	   q
+
+ASCII(d) = ASCII(c) + 1 -26*k
+ASCII(c) = ASCII(d) - 1 + 26*k
+c = [d - 1] range of a to z
+c = [d - 1 + 26] range of a to z
+
+ascii(n) = ascii(r) + ascii(c) + 1 - 26*k
+ascii(r) = ascii(n) - ascii(c) - 1 + 26*k
+ascii(r) = ascii(n) - ASCII(d)
+*/
