df40b4e092237e1bb00ad261aed0108ba03052a4
kumag <kumar420gaurav@gmail.com>
2020-02-23 20:10:39 +0530

    Update Subsets.java

--- Subsets.java
+++ Subsets.java
@@ -20,3 +20,43 @@ class Solution {
         fun(nums, topList, newList, n + 1);   
     }
 }
+//iterative
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> topList =  new LinkedList<>();
+        
+        for(int i = 0; i < Math.pow(2, nums.length); i++){
+            fun(i, topList, nums);
+        }
+        return topList;
+    }
+    
+    public void fun(int n, List<List<Integer>> topList, int[] nums){
+        String s = IntegertoBinaryString(n, nums.length);
+        //System.out.println(s);
+        List<Integer> list = new LinkedList<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '1'){
+                list.add(nums[i]);
+            }
+        }
+        
+        topList.add(list);
+    }
+    
+    public String IntegertoBinaryString(int num, int n){
+        String str = "";
+        for(int i = 1; i <= n; i++){
+            if((num & 1) == 1){
+                str = '1' + str;
+            }
+            else{
+                str = '0' + str;
+            }
+            num >>= 1;
+        }
+        return str;
+    }
+}
