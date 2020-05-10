9788777e97f3722de18a054fe311a7654d22f748
kumag <kumar420gaurav@gmail.com>
2020-05-10 18:09:32 +0530

    Create Middle of the Linked List.js

---
+++ Middle of the Linked List.js
@@ -0,0 +1,24 @@
+//Middle of the Linked List
+//https://leetcode.com/problems/middle-of-the-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow = head;
+    let fast = head;
+    
+    while(fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+};
