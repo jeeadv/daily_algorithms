17fa1b84c32a37c94a9c340feceb9c2623832f73
kumag <kumar420gaurav@gmail.com>
2020-05-19 18:23:48 +0530

    Update Print FooBar Alternately.java

--- Print FooBar Alternately.java
+++ Print FooBar Alternately.java
@@ -115,3 +115,33 @@ class FooBar {
         }
     }
 }
+
+class FooBar {
+    private int n;
+    private Semaphore foo = new Semaphore(1);
+    private Semaphore bar = new Semaphore(0);
+    
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            
+            foo.acquire();
+        	printFoo.run();
+            bar.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            
+            bar.acquire();
+        	printBar.run();
+            foo.release();
+        }
+    }
+}
