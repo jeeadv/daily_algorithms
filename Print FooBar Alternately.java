126ebb169c8a2f57c35cae69826bdbf265b10470
kumag <kumar420gaurav@gmail.com>
2020-05-19 17:27:30 +0530

    Update Print FooBar Alternately.java

--- Print FooBar Alternately.java
+++ Print FooBar Alternately.java
@@ -37,3 +37,81 @@ class FooBar {
         }
     }
 }
+
+
+class FooBar {
+    private int n;
+    private boolean even = true;
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public synchronized void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            
+        	if(!this.even) {
+                this.wait();
+            }
+            
+            printFoo.run();
+            this.even = !this.even;
+            this.notify();
+        }
+    }
+
+    public synchronized void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            
+            if(this.even) {
+                this.wait();
+            }
+            
+            printBar.run();
+            this.even = !this.even;
+            this.notify();
+        }
+    }
+}
+
+
+class FooBar {
+    private int n;
+    private boolean even = true;
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            synchronized(this) {
+                if(!this.even) {
+                    this.wait();
+                }
+
+                printFoo.run();
+                this.even = !this.even;
+                this.notify();
+            }
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            synchronized(this) {
+                if(this.even) {
+                    this.wait();
+                }
+
+                printBar.run();
+                this.even = !this.even;
+                this.notify();
+            }
+        }
+    }
+}
