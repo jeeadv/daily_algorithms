57b6c7d91017252293efa765d7a770f6c6bd5377
kumag <kumar420gaurav@gmail.com>
2020-05-20 17:46:21 +0530

    Update Fizz Buzz Multithreaded.java

--- Fizz Buzz Multithreaded.java
+++ Fizz Buzz Multithreaded.java
@@ -65,3 +65,94 @@ class FizzBuzz {
         return !onlyDivisibleBy3(i) && !onlyDivisibleBy5(i) && !divisibleBy3And5(i);
     }
 }
+
+
+
+class FizzBuzz {
+    private int n;
+    private int i = 1;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        while(i <= n) {
+            synchronized(this){
+                if(onlyDivisibleBy3(i)) {
+                    printFizz.run();
+                    i++;
+                    this.notifyAll();
+                }
+                else {
+                    this.wait();
+                }
+            }
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        while(i <= n) {
+            synchronized(this){
+                if(onlyDivisibleBy5(i)) {
+                    printBuzz.run();
+                    i++;
+                    this.notifyAll();
+                }
+                else {
+                    this.wait();
+                }
+            }
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        while(i <= n) {
+            synchronized(this){
+                if(divisibleBy3And5(i)) {
+                    printFizzBuzz.run();
+                    i++;
+                    this.notifyAll();
+                }
+                else {
+                    this.wait();
+                }
+            }
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        while(i <= n) {
+            synchronized(this){
+                if(divisibleByOther(i)) {
+                    printNumber.accept(i);
+                    i++;
+                    this.notifyAll();
+                }
+                else {
+                    this.wait();
+                }
+            }
+        }
+    }
+    
+    public boolean onlyDivisibleBy3(int i) {
+        return i % 3 == 0 && i % 5 != 0;
+    }
+    
+    public boolean onlyDivisibleBy5(int i) {
+        return i % 3 != 0 && i % 5 == 0;
+    }
+    
+    public boolean divisibleBy3And5(int i) {
+        return i % 3 == 0 && i % 5 == 0;
+    }
+    
+    public boolean divisibleByOther(int i) {
+        return !onlyDivisibleBy3(i) && !onlyDivisibleBy5(i) && !divisibleBy3And5(i);
+    }
+}
