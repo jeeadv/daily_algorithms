a4bf5a1f3ebe16b98ce6af23c4af849f169672cc
kumag <kumar420gaurav@gmail.com>
2020-05-24 16:26:30 +0530

    Update Print Zero Even Odd.java

--- Print Zero Even Odd.java
+++ Print Zero Even Odd.java
@@ -1,6 +1,8 @@
 //Print Zero Even Odd
 //https://leetcode.com/problems/print-zero-even-odd/
 
+// wait() notify() monitor
+
 class ZeroEvenOdd {
     private int n;
     private int i = 1;
@@ -59,6 +61,70 @@ class ZeroEvenOdd {
                 else {
                     this.wait();
                 }
+            }
+        }
+    }
+}
+
+// Semaphore
+
+class ZeroEvenOdd {
+    private int n;
+    private int i = 1;
+    private Semaphore sZero = new Semaphore(1);
+    private Semaphore sOdd = new Semaphore(0);
+    private Semaphore sEven = new Semaphore(0);
+    
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        while(i <= n) {
+            sZero.acquire();
+            if(i <= n) {
+                printNumber.accept(0);
+                if(i % 2 == 0) {
+                    sEven.release();
+                }
+                else {
+                    sOdd.release();
+                }
+            }
+            else {
+                sEven.release();
+                sOdd.release();
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        while(i <= n) {
+            sEven.acquire();
+            if(i <= n) {
+                printNumber.accept(i);
+                i++;
+                sZero.release();
+            }
+            else {
+                sZero.release();
+                sOdd.release();
+            }
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        while(i <= n) {
+            sOdd.acquire();
+            if(i <= n) {
+                printNumber.accept(i);
+                i++;
+                sZero.release();
+            }
+            else {
+                sZero.release();
+                sEven.release();
             }
         }
     }
