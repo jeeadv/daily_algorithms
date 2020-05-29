ca1bfa5e0cbcecca5315d56010c5996d57904db3
kumag <kumar420gaurav@gmail.com>
2020-05-29 13:44:21 +0530

    Update Diameter of Binary Tree2.java

--- Diameter of Binary Tree2.java
+++ Diameter of Binary Tree2.java
@@ -1,6 +1,7 @@
 //Diameter of Binary Tree
 //https://practice.geeksforgeeks.org/problems/diameter-of-binary-tree/1
 //https://www.geeksforgeeks.org/diameter-of-a-binary-tree/
+//https://www.geeksforgeeks.org/diameter-of-a-binary-tree-in-on-a-new-method/
 
 // { Driver Code Starts
 // Initial Template for Java
@@ -127,3 +128,137 @@ class Tree {
         return new int[] {maxDia, maxHeight};
     }
 }
+
+
+--------------------------------------------
+    // { Driver Code Starts
+// Initial Template for Java
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Tree g = new Tree();
+            System.out.println(g.diameter(root));
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Tree {
+    /* Complete the function to get diameter of a binary tree */
+    public static int max = 0;
+    
+    int diameter(Node root) {
+        max = 0;
+        height(root);
+        return max;
+    }
+    
+    int height(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        
+        int left = height(root.left);
+        int right = height(root.right);
+        
+        if(max < left + right + 1) {
+            max = left + right + 1;
+        }
+        
+        return Math.max(left, right) + 1;
+    }
+}
+
