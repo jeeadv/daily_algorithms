1a9a795d925818018070fe1e847b8ee06a1b18ce
kumag <kumar420gaurav@gmail.com>
2020-05-31 16:32:18 +0530

    Update Edit Distance.java

--- Edit Distance.java
+++ Edit Distance.java
@@ -29,3 +29,48 @@ class Solution {
         }
     }
 }
+
+// Memomization with recursion
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m][n];
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        
+        return minDistance(0, 0, word1, word2, dp);
+    }
+    
+    public int minDistance(int i, int j, String a, String b, int[][] dp) {
+        if(i == a.length() && j == b.length()) {
+            return 0;
+        }
+        else if(i == a.length() && j < b.length()) {
+            return 1 + minDistance(i, j + 1, a, b, dp);
+        }
+        else if(i < a.length() && j == b.length()) {
+            return 1 + minDistance(i + 1, j, a, b, dp);
+        }
+        
+        if(dp[i][j] != Integer.MAX_VALUE) {
+            return dp[i][j];
+        }
+        
+        if(a.charAt(i) == b.charAt(j)) {
+            return minDistance(i + 1, j + 1, a, b, dp);
+        }
+        else {
+            int way1 = minDistance(i, j + 1, a, b, dp); //insert
+            int way2 = minDistance(i + 1, j + 1, a, b, dp); //replace
+            int way3 = minDistance(i + 1, j, a, b, dp); // delete
+            
+            return dp[i][j] = 1 + Math.min(way1, Math.min(way2, way3));
+        }
+    }
+}
