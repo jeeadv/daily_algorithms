9c35681ee9f6f9675079fd1dfff8f2e7c6e4493b
kumag <kumar420gaurav@gmail.com>
2020-06-01 16:08:23 +0530

    Create Invert Binary Tree Leetcode.java

---
+++ Invert Binary Tree Leetcode.java
@@ -0,0 +1,20 @@
+//Invert Binary Tree
+//https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3347/
+
+//preorder
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) {
+            return root;
+        }
+        
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        root.left = invertTree(root.left);
+        root.right = invertTree(root.right);
+        
+        return root;
+    }
+}
