570362090d77161192171981f41a99d2f9ee5031
kumag <kumar420gaurav@gmail.com>
2020-06-01 16:10:25 +0530

    Update Invert Binary Tree Leetcode.java

--- Invert Binary Tree Leetcode.java
+++ Invert Binary Tree Leetcode.java
@@ -18,3 +18,21 @@ class Solution {
         return root;
     }
 }
+
+// postorder
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) {
+            return root;
+        }
+        
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        
+        root.left = right;
+        root.right = left;
+        
+        return root;
+    }
+}
