90e8c5b7d8ac8af35c3f0e4885ca2f9f67c10b1e
kumag <kumar420gaurav@gmail.com>
2020-06-04 01:31:25 +0530

    Update Trapping Rain Water.java

--- Trapping Rain Water.java
+++ Trapping Rain Water.java
@@ -1,6 +1,7 @@
 //Trapping Rain Water
 //https://leetcode.com/problems/trapping-rain-water/
 
+// dp O(n) O(n)
 class Solution {
     public int trap(int[] height) {
         
@@ -24,6 +25,39 @@ class Solution {
         int res = 0;
         for(int i = 0; i < height.length; i++) {
             res += Math.min(prev[i], next[i]) - height[i];
+        }
+        
+        return res;
+    }
+}
+
+// two pointer O(n) O(1)
+class Solution {
+    public int trap(int[] height) {
+        
+        if(height.length < 3) {
+            return 0;
+        }
+        
+        int leftMax = 0;
+        int rightMax = 0;
+        
+        int l = 0;
+        int r = height.length - 1;
+        int res = 0;
+        while(l <= r) {
+            
+            leftMax = Math.max(leftMax, height[l]);
+            rightMax = Math.max(rightMax, height[r]);
+            
+            if(height[l] <= height[r]) {
+                res += Math.min(leftMax , rightMax) - height[l];
+                l++;
+            }
+            else {
+                res += Math.min(leftMax , rightMax) - height[r];
+                r--;
+            }
         }
         
         return res;
