14ba19333b79181338c24bebf74eea9a44d4f7ff
kumag <kumar420gaurav@gmail.com>
2020-06-04 14:51:04 +0530

    Create Find the Town Judge.java

---
+++ Find the Town Judge.java
@@ -0,0 +1,22 @@
+//Find the Town Judge
+//https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3325/
+
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        int[] knownTo = new int[N + 1];
+        int[] knows = new int[N + 1];
+        
+        for(int i = 0; i < trust.length; i++) {
+            knownTo[trust[i][1]]++;
+            knows[trust[i][0]]++;
+        }
+        
+        for(int i = 1; i <= N; i++) {
+            if(knownTo[i] == N - 1 && knows[i] == 0) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
