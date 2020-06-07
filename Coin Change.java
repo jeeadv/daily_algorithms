33e55337c62450597c25cee8ba0c83689bdee946
kumag <kumar420gaurav@gmail.com>
2020-06-07 14:42:29 +0530

    Update Coin Change.java

--- Coin Change.java
+++ Coin Change.java
@@ -76,3 +76,24 @@ class Solution {
         return arr[amount] == Integer.MAX_VALUE ? -1 : arr[amount];
     }
 }
+
+// short
+
+class Solution {
+    
+    public int coinChange(int[] coins, int amount) {
+        int[] arr = new int[amount + 1];
+        Arrays.fill(arr, amount + 1);
+        arr[0] = 0;
+        
+        for(int i = 1; i <= amount; i++) {
+            for(int j = 0; j < coins.length; j++) {
+                if(i - coins[j] >= 0) {
+                    arr[i] = Math.min(arr[i] , arr[i - coins[j]] + 1);
+                }
+            } 
+        }
+        
+        return arr[amount] == amount + 1 ? -1 : arr[amount];
+    }
+}
