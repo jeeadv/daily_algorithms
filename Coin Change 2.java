d9d8abaecb94bfd61fa8450276da370ad062201c
kumag <kumar420gaurav@gmail.com>
2020-06-07 21:04:57 +0530

    Update Coin Change 2.java

--- Coin Change 2.java
+++ Coin Change 2.java
@@ -47,3 +47,56 @@ class Solution {
     }
     
 }
+
+// all possibility print
+
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoinChange {
+
+    static List<String>[] list;
+    
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		int amount = 5;
+		int[] coins = {1, 2, 5};
+		
+		list = new ArrayList[amount + 1];
+		
+		System.out.println(new CoinChange().change(amount, coins));
+		
+		for(List<String> item: list) {
+			System.out.println(item);
+		}
+	}
+	
+	public int change(int amount, int[] coins) {
+        int[] arr = new int[amount + 1];
+        arr[0] = 1;
+        
+        for(int coin: coins) {
+            for(int i = coin; i <= amount; i++) {
+                arr[i] += arr[i - coin];
+                
+                /* adding start*/
+                if(list[i] == null) {
+                	list[i] = new ArrayList<>();
+                }
+                if(list[i - coin] == null) {
+                	list[i].add(String.valueOf(i));
+                }
+                else {
+                	for(String s: list[i - coin]) {
+                		list[i].add(s + " " + coin);
+                	}
+                }
+                /* adding end*/
+            }
+        }
+        return arr[amount];
+    }
+
+}
