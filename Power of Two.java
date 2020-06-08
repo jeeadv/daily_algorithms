7dfd8a2127d1bf06200a45ae2ec551757e57c4aa
kumag <kumar420gaurav@gmail.com>
2020-06-08 14:56:06 +0530

    Update Power of Two.java

--- Power of Two.java
+++ Power of Two.java
@@ -24,3 +24,22 @@ class Solution {
         return isPowerOfTwo(n / 2);
     }
 }
+
+// another bit manipulation
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0) {
+            return false;
+        }
+        
+        int count = 0;
+        for(int i = 0; i < 32; i++) {
+            if((n & 1) == 1) {
+                count++;
+            }
+            n >>= 1;
+        }
+        return count == 1;
+    }
+}
