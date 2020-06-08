f458a50eeb9faddd92f845772e11ef2bb11fdd01
kumag <kumar420gaurav@gmail.com>
2020-06-08 14:50:07 +0530

    Update Power of Two.java

--- Power of Two.java
+++ Power of Two.java
@@ -1,8 +1,26 @@
 //Power of Two
 //https://leetcode.com/problems/power-of-two/
 
+// bit manipulation
 class Solution {
     public boolean isPowerOfTwo(int n) {
         return n > 0 && (n & (n - 1)) == 0;
+    }
+}
+
+// recursive
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0) {
+            return false;
+        }
+        else if(n == 1) {
+            return true;
+        }
+        else if(n % 2 == 1) {
+            return false;
+        }
+        return isPowerOfTwo(n / 2);
     }
 }
