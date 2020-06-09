0e60d82972cf3a5832a8084dc185015017a8ca30
kumag <kumar420gaurav@gmail.com>
2020-06-09 12:56:57 +0530

    Create Power of Four.java

---
+++ Power of Four.java
@@ -0,0 +1,8 @@
+//Power of Four
+//https://leetcode.com/problems/power-of-four/
+
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        return num > 0 && (Math.log(num) / Math.log(2)) % 2 == 0;
+    }
+}
