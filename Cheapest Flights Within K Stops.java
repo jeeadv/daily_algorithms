9f05249a1265e03e80ba5fe9a4992a70edce5cc1
kumag <kumar420gaurav@gmail.com>
2020-06-15 10:45:40 +0530

    Update Cheapest Flights Within K Stops.java

--- Cheapest Flights Within K Stops.java
+++ Cheapest Flights Within K Stops.java
@@ -1,6 +1,7 @@
 //Cheapest Flights Within K Stops
 //https://leetcode.com/problems/cheapest-flights-within-k-stops/
 
+// adj matrix
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
         int[][] graph = new int[n][n];
@@ -44,5 +45,49 @@ class Solution {
             }
         }
         return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
+}
+
+
+// adj list
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+        List<List<Integer>>[] graph = new ArrayList[n];
+        
+        for(int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for(int[] arr: flights) {
+            List<Integer> list = new ArrayList<>();
+            list.add(arr[1]);
+            list.add(arr[2]);
+            graph[arr[0]].add(list); 
+        }
+        
+        Queue<int[]> q = new PriorityQueue<>(new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+        q.add(new int[] {0, src, -1});
+        
+        while(!q.isEmpty()) {
+            int[] top = q.remove();
+            int wt = top[0];
+            int u = top[1];
+            int k = top[2];
+            
+            if(u == dst) {
+                return wt;
+            }
+           
+            if(k < K) {
+                for(List<Integer> p: graph[u]) {
+                    q.add(new int[] {wt + p.get(1), p.get(0), k + 1});
+                }   
+            }
+        }
+        return -1;
     }
 }
