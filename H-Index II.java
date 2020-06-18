c818118f2e4445d6140e833933bf204937f218f5
kumag <kumar420gaurav@gmail.com>
2020-06-18 20:42:12 +0530

    Create H-Index II.java

---
+++ H-Index II.java
@@ -0,0 +1,13 @@
+//H-Index II
+//https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/541/week-3-june-15th-june-21st/3364/
+
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int max = 0;
+        for(int i = 0; i < n; i++) {
+            max = Math.max(max, Math.min(citations[i],  n - i));
+        }
+        return max;
+    }
+}
