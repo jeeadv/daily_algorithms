26fcfa0fa12c6847d2949064733dd49222b896e6
kumag <kumar420gaurav@gmail.com>
2020-06-18 21:16:32 +0530

    Update H-Index II.java

--- H-Index II.java
+++ H-Index II.java
@@ -13,3 +13,28 @@ class Solution {
     }
 }
 
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        //System.out.println(Arrays.toString(citations));
+        int n = citations.length;
+        int max = 0;
+        int l = 0;
+        int r = n - 1;
+        
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            int val = Math.min(citations[mid], n - mid);
+            //System.out.println(citations[mid] + " " + (n - mid));
+            //System.out.println(l + " " + r + " " + mid + " " + val);
+            max = Math.max(max, Math.min(citations[mid], n - mid));
+            if(citations[mid] <= (n - mid)) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return max;
+    }
+}
