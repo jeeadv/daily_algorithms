6c7fcc1116f5930d221ba9d00bb07ec102fc4935
kumag <kumar420gaurav@gmail.com>
2020-07-25 11:57:34 +0530

    Update All Paths From Source to Target.java

--- All Paths From Source to Target.java
+++ All Paths From Source to Target.java
@@ -1,6 +1,7 @@
 //All Paths From Source to Target
 //https://leetcode.com/problems/all-paths-from-source-to-target/
 
+//dfs
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         List<List<Integer>> res = new ArrayList<>();
@@ -29,4 +30,29 @@ class Solution {
         }
         return list;
     }
+}
+
+
+// solution with array list copy (dfs)
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        dfs(graph, 0, path, res);
+        return res;
+    }
+    
+    public void dfs(int[][] graph, int u, List<Integer> path, List<List<Integer>> res) {
+        path.add(u);
+        if(u == graph.length - 1) {
+            res.add(path);
+            return;
+        }
+       
+        for(int v: graph[u]) {
+            List<Integer> copyList = new ArrayList<>(path);
+            dfs(graph, v, copyList, res);
+        }
+    }
+    
 }
